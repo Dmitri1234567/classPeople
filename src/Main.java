2c84c1684751a7d2c30244cfafcfb8d32ca289b0
Dima <dmitrijskurcenko3@gmail.com>
2022-09-28 12:51:36 +0300

    classPeople
    classPeople

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author admin
+ */
+public class Main {
+    
+  public static void main(String[] args)
+{
+peopleClass people=new peopleClass();
+    
+people.setFirstName("John");
+people.setLastName("Hight");
+people.setYear(1990);
+people.setMonth(7);
+    peopleClass people1=new peopleClass();
+    people1.setFirstName("Kolia");
+    people1.setLastName("polia");
+    people1.setMonth(8);
+    people1.setYear(1991);
+    
+    peopleClass people2=new peopleClass();
+    people2.setFirstName("Lion");
+    people2.setLastName("Kraik");
+    people2.setMonth(9);
+    people2.setYear(1992);
+System.out.println(people1.getMonth());
+System.out.println(people2.getMonth());
+
+    
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+    
+
---
+++ src/peopleClass.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author admin
+ */
+public class peopleClass {
+    
+private String firstName;
+private String lastName;
+private int year;
+private int month;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+  
+    
+
+  
+}
+
+
+ 
+
+    
